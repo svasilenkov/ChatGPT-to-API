4f471d3a3fbe9f1b390a81e2c45cb345c3b03442
Sergey Vasilenkov <sergey@munsoft.com>
2023-05-17 22:25:13 +0400

    Don't use defatul 127.0.0.1 host

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func init() {
 	HOST = os.Getenv("SERVER_HOST")
 	PORT = os.Getenv("SERVER_PORT")
 	if HOST == "" {
-		HOST = "127.0.0.1"
+		//HOST = "127.0.0.1"
 	}
 	if PORT == "" {
 		PORT = "8080"
