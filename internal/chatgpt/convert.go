b58afec9a641aecc4c355344f6b381eb86304c48
Sergey Vasilenkov <sergey@munsoft.com>
2023-05-18 16:23:36 +0400

    Fix

--- internal/chatgpt/convert.go
+++ internal/chatgpt/convert.go
@@ -7,7 +7,7 @@ import (
 func ConvertAPIRequest(api_request typings.APIRequest) typings.ChatGPTRequest {
 	chatgpt_request := typings.NewChatGPTRequest(api_request.Model)
 	for _, api_message := range api_request.Messages {
-		if api_message.Role == "system" {
+		if api_request.Model != "gpt4" && api_message.Role == "system" {
 			api_message.Role = "critic"
 		}
 		chatgpt_request.AddMessage(api_message.Role, api_message.Content)
